b04c6a37f8d86b3d373ecb4dda51b1020def1a70
BuildTools <unconfigured@null.spigotmc.org>
2020-05-06 11:05:56 +0200

    ex2

---
+++ ex2/main.js
@@ -0,0 +1,20 @@
+function main(){
+    let d = document.querySelector("#afterTable");
+    let titre = document.title;
+    let nbLigne = document.getElementsByTagName("tr").length;
+    let nbColonne = (document.getElementsByTagName("td").length+document.getElementsByTagName("th").length)/nbLigne;
+    let nbSemestre = document.getElementsByClassName("newSemestre").length;
+    let noteS5E1 = document.querySelectorAll("tr")[9].getElementsByTagName("td")[2].innerText
+    let noteS5E2 = document.querySelectorAll("tr")[10].getElementsByTagName("td")[2].innerText
+
+    d.innerText = "Titre : "+titre+"\nNombre de lignes : "+nbLigne+"\nNombre de colonnes : "+nbColonne+"\nNombre de semestres : "+nbSemestre+"\nNote de S5 de E1 : "+noteS5E1+"\nNote de S5 de E2 : "+noteS5E2+"\n";
+
+    document.querySelectorAll("tr")[9].getElementsByTagName("td")[2].innerText = parseInt(noteS5E1)+2;
+    document.querySelectorAll("tr")[9].getElementsByTagName("td")[2].style.fontSize = "25px";
+    document.querySelectorAll("tr")[9].getElementsByTagName("td")[2].style.color = "red";
+
+    let zoneTxt = document.createElement("textarea");
+    d.appendChild(zoneTxt);
+}
+
+main();
