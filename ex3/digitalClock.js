d838438b0c17c9b0ce7fbc298206f52fb0557562
BuildTools <unconfigured@null.spigotmc.org>
2020-05-06 11:44:11 +0200

    ex3

---
+++ ex3/digitalClock.js
@@ -0,0 +1,63 @@
+﻿function addSegments(digitId){
+    let divDigit = document.getElementById(digitId);
+    
+    for(let i = 0; i <= 6; i++){
+        let segment = document.createElement("div");
+        segment.className = "segment off segment"+i;
+        divDigit.appendChild(segment);
+    }
+
+}
+
+function updateDigit(digitId, value){
+
+    let segmentStates = [
+        [1, 1, 1, 0, 1, 1, 1],
+        [0, 0, 1, 0, 0, 1, 0],
+        [1, 0, 1, 1, 1, 0, 1],
+        [1, 0, 1, 1, 0, 1, 1],
+        [0, 1, 1, 1, 0, 1, 0],
+        [1, 1, 0, 1, 0, 1, 1],
+        [1, 1, 0, 1, 1, 1, 1],
+        [1, 0, 1, 0, 0, 1, 0],
+        [1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 0, 1, 1]
+    ];
+
+    for(let i = 0; i <= 6; i++){
+        if(segmentStates[value][i] == 1){
+            document.querySelectorAll("#"+digitId+" > .segment"+i)[0].className = "segment segment"+i;
+        }else{
+            document.querySelectorAll("#"+digitId+" > .segment"+i)[0].className = "segment off segment"+i;
+        }
+    }
+}
+
+
+
+
+function init(){
+    addSegments("hours-tens");
+    addSegments("hours-units");
+    addSegments("minutes-tens");
+    addSegments("minutes-units");
+}
+
+
+
+function main(){
+
+    init();
+
+    setInterval(function() {
+        var d = new Date();
+        updateDigit("hours-tens", Math.trunc(d.getHours()/10));
+        updateDigit("hours-units", d.getHours()%10);
+        updateDigit("minutes-tens", Math.trunc(d.getMinutes()/10));
+        updateDigit("minutes-units", d.getMinutes()%10);
+    }, 1000);
+
+}
+
+
+main();
